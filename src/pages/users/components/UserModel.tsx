3f38590cb031e37a170e2309fc0d5f9dffb33092
deng <q921598334@126.com>
2021-04-04 09:59:54 +0800

    11课，完成选择不同的项目，对话框回显不同的项目数据

--- src/pages/users/components/UserModel.tsx
+++ src/pages/users/components/UserModel.tsx
@@ -1,18 +1,87 @@
-import React from 'react'
-import { Modal, Button } from 'antd';
+import React,{useEffect} from 'react'
+import { Modal, Button, Form, Input } from 'antd';
 
 const UserModel = (props) => {
 
-    var isModalVisible = true
+    const [form] = Form.useForm();
+
+    console.log('选择的record:')
+    console.log(props.record)
+
+    //第一个参数是要执行的函数，第二个参数是一个列表，列表中的值变化时执行函数
+    useEffect(()=>{
+        form.setFieldsValue(props.record)
+    },[props.record])
+
+    
 
     return (
-        <Modal title="Basic Modal"
+        <Modal 
+            title="Basic Modal"
             visible={props.visible}
             onOk={props.handleOK}
             onCancel={props.handleCancel}>
-            <p>Some contents...</p>
-            {JSON.stringify(props.record)}
-            
+            forceRender
+            {/* {JSON.stringify(props.record)} */}
+
+            <Form
+                form={form}
+                // {...layout}
+                name="basic"
+                // initialValues={{ 
+                //     remember: true,
+                //     name:'张',
+                // }}
+            // onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    label="姓名"
+                    name="name"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="年龄"
+                    name="age"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="地址"
+                    name="address"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+
+
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+
+
+                <Form.Item
+                // {...tailLayout}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+
+
+
         </Modal>
     )
 }
